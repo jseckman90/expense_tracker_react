45b09f6c2e40638bfa9c15e6095c1f99ede06687
Josh Seckman <jseckman90@gmail.com>
2021-01-06 20:15:32 -0500

    set up state in AddTransaction component

--- src/components/AddTansaction.js
+++ src/components/AddTansaction.js
@@ -1,20 +1,38 @@
 import React from "react";
 
 const AddTransaction = () => {
+  const [text, setText] = React.useState("");
+  const [amount, setAmount] = React.useState(0);
   return (
     <>
       <h3>Add new transaction</h3>
       <form id="form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+            id="text"
+            placeholder="Enter text..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(event) => {
+              setAmount(event.target.value);
+            }}
+            id="amount"
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
