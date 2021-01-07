073f15cab1749636a07738fc714deb8cf84729a4
Josh Seckman <jseckman90@gmail.com>
2021-01-06 19:57:00 -0500

    created income/expenses component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 function App() {
   return (
     <div>
       <Header />
-      <Balance />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
