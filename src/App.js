2179c9982ebd952afbbae039b12fa41a1f77a388
Josh Seckman <jseckman90@gmail.com>
2021-01-07 21:24:49 -0500

    displaying transaction text

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Header from "./components/Header";
 import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
-import AddTransaction from "./components/AddTansaction";
+import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider } from "./AppState";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -15,7 +16,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
---
+++ src/AppState.js
@@ -0,0 +1,27 @@
+import React from "react";
+import AppReducer from "./AppReducer";
+
+// initial state
+const initialState = {
+  transactions: [
+    { id: 1, text: "computer", amount: -2000 },
+    { id: 2, text: "freelance", amount: 1200 },
+    { id: 3, text: "monitor", amount: -200 },
+    { id: 4, text: "paycheck", amount: 1500 },
+  ],
+};
+
+// creat context
+
+export const GlobalContext = React.createContext(initialState);
+
+// context provider
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(AppReducer, initialState);
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
